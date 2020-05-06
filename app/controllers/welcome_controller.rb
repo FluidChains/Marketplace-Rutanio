913ffe55e62e345585a8e0b78ba7d28b7b8df142
Sebas <jsagudeloaa@gmail.com>
2020-05-06 03:55:54 -0500

    correccion front end formulario de creacion nuevo servicio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @services = Service.paginate(:page => params[:page], :per_page => 24)
     @service = Service.new
-    
+  end
+  def postulaciones
+    @requests = current_user.requests
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   get 'welcome/index'
+  get 'welcome/postulaciones'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
