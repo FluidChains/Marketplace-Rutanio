3bb2bc1ae1508ded3479301383e9b59290aac029
Sebas <jsagudeloaa@gmail.com>
2020-05-17 00:31:53 -0500

    correcion boton compart de facebook -se agrega compartir en reddit

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter linkedin whatsapp_app whatsapp_web)
+  config.allow_sites = %w(facebook twitter linkedin reddit whatsapp_app whatsapp_web )
 end
