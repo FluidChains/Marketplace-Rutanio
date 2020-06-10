303f085eb04177d003fda3f8129a94fc2add9644
Sebas <jsagudeloaa@gmail.com>
2020-06-10 06:07:59 -0500

    correcion buscador

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+class SearchController < ApplicationController
+  def create
+    palabra = "%#{params[:keyword]}%"
+    @services = Service.publicados.where("name LIKE ? OR description LIKE ? ",palabra,palabra)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: @services }
+      format.js 
+    end
+  end
+end
