2eadd604ccabedcb8f24f2ff7cb6509f9b717758
Sebas <jsagudeloaa@gmail.com>
2020-07-26 07:28:49 -0500

    nuevo template para mailing de servicios

--- app/mailers/requests_mailer.rb
+++ app/mailers/requests_mailer.rb
@@ -3,11 +3,11 @@ class RequestsMailer < ApplicationMailer
 
   def formulariorequest(contacto)
     @contacto = contacto.mensaje
-    @greeting = "Felicidades  #{contacto.user.email} te postulaste con exito a la publicacion de #{contacto.service.mail_servicio}"
+    @greeting = "Felicidades  #{contacto.user.email} se postulo con exito a la publicacion de #{contacto.service.mail_servicio}, visita tu perfil del marketplace y sigue creando nuevas alianzas"
 
       @recipients =   contacto.service.mail_servicio
       emails = @recipients
-      mail(:to => emails, :subject => "A replacement clerk has been requested #{@greeting} ")
+      mail(:to => emails, :subject => " #{@greeting} ")
 
 
   end
