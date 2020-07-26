a4f18cceb47f4695a8edfb4d8ffff8f279fc6b26
Sebas <jsagudeloaa@gmail.com>
2020-07-26 09:39:53 -0500

    modificacion template de correo se agrego el envio a los dos involucrados de cada respuesta a servicio

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -4,9 +4,9 @@ class UserNotifierMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(contacto)
     @contacto = contacto.mensaje
-    @greeting = "Felicidades  #{contacto.user.email} se postulo con exito a la publicacion #{@contacto} de #{contacto.service.mail_servicio}"
-      @recipients =   contacto.service.mail_servicio
+    @greeting = "Felicidades  #{contacto.user.email} se postulo con exito a la publicacion de #{contacto.service.mail_servicio}"
+      @recipients =  "  #{contacto.user.email}, #{contacto.service.mail_servicio}"
       emails = @recipients
-      mail(:to => emails, :subject => "Notificacion Rutanio Marketplace #{@greeting} ")
+      mail(:to => emails, :subject => "Notificacion Rutanio Marketplace")
   end
 end
