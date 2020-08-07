3865635265f5c42cba4d896b3cbe3d6d01a7cf8b
checho1989 <cheverdolaga@hotmail.com>
2020-08-07 02:02:32 -0400

    Actualizacion variables de entorno

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,14 +96,14 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'marketplace.rutanio.com' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
 
   #Use a different logger for distributed setups.
 
   ActionMailer::Base.smtp_settings = {
 #    :user_name => ENV['SENDGRID_USERNAME'],
 #    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://marketplace.rutanio.com',
+    :domain => ENV['SMTP_DOMAIN'],
 #    :address => 'smtp.sendgrid.net',
 #    :port => 587,
 #    :authentication => :plain,
