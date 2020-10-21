3f02598961d2f5320aee62bc2e98df9c9499afde
Sebas <jsagudeloaa@gmail.com>
2020-10-21 03:48:48 -0500

    prueba heroku ambiente produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'marketplace.rutanio.com' }
+  config.action_mailer.default_url_options = { :host => 'rutanio.herokuapp.com' }
 
   #Use a different logger for distributed setups.
 
