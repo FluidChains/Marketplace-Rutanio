e51cbe8091c398c05eefe88fa8850be6f987e363
sebastian agudelo <s.agudelo@rutanmedellin.org>
2020-11-21 05:07:09 -0500

    sin especificar dominio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
 
-  config.hosts << '.marketplace.rutanio.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
