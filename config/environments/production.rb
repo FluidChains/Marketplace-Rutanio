1b458535c3d5c7606e684b8505bb3cf95303b005
sebastian agudelo <s.agudelo@rutanmedellin.org>
2020-11-21 05:00:32 -0500

    Blocked host: probando con .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.hosts << "marketplace.rutanio.com"
+
+  config.hosts << '.marketplace.rutanio.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -96,7 +97,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  
+
 
   #Use a different logger for distributed setups.
 
