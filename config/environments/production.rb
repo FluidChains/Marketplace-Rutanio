179816425bea7d0e8f024e4b40e15d366d11036d
sebastian agudelo <s.agudelo@rutanmedellin.org>
2020-11-21 04:55:09 -0500

    modificacion hosti production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'marketplace.rutanio.com' }
+  
 
   #Use a different logger for distributed setups.
 
